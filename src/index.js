e582e8467588a616f1d8a7a3a2c0729322ec2aee
jonathanx111 <jonathanx111@gmail.com>
2021-01-12 01:41:58 -0500

    final

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import ReactDOM from 'react-dom';
 import Keypad from './components/Keypad';
 import EyesOnMe from './components/EyesOnMe';
 
+function handleChange() {
+  console.log("Entering password...")
+}
+
+function handleFocus() {
+  console.log("Good!")
+}
+
+function handleBlur() {
+  console.log("Hey! Eyes on me!")
+}
+
 ReactDOM.render(
   <div>
     <Keypad />
